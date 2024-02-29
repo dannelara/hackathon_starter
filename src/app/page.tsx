047dd3b6e2eb1124a888ec7aa07150d60a9128ce
Daniel Lara <11daniel@live.se>
2024-02-29 18:09:53 +0100

    updated title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ import ImageGenerator from "./examples/imageGenerator/ImageGenerator";
 export default function Home() {
   return (
     <main className="min-h-screen p-24 space-y-8">
-      <h1 className="text-5xl">AI starter kit</h1>
+      <h1 className="text-5xl">Starter kit</h1>
       <h2 className="text-xl">Ni ska i teams av 2 komma på en idé på något roligt ni vill utveckla!</h2>
-      <p>Jag har förberett en startmall för er, som inkluderar tre olika AI-exempelidéer. Ni har tillgång till all kod för att använda som grund. Valet är ert: ni kan antingen implementera något eget och unikt, eller bygga vidare på de exempel som redan finns med.</p>
+      <p>Jag har förberett en startmall för er, som inkluderar tre olika exempelidéer. Ni har tillgång till all kod för att använda som grund. Valet är ert: ni kan antingen implementera något eget och unikt, eller bygga vidare på de exempel som redan finns med.</p>
       <div className="h-screen" />
       <p className="underline text-6xl pb-12">Exempel:</p>
       <div className="grid gap-20">
