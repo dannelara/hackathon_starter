4af07328d25f08f562268759a3aabb7394ed8deb
Daniel Lara <11daniel@live.se>
2024-02-29 17:35:30 +0100

    cn

--- src/app/components/forms/ChatForm.tsx
+++ src/app/components/forms/ChatForm.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import Input from '../ui/Input';
+import { cn } from '@/app/utils';
 
 type Props = {
     input: string;
     handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
     handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+    className?: string;
 }
 
 
-const ChatForm = ({ handleInputChange, handleSubmit, input }: Props) => {
+const ChatForm = ({ handleInputChange, handleSubmit, input, className }: Props) => {
     return (
-        <form onSubmit={handleSubmit} className='mt-4'>
+        <form onSubmit={handleSubmit} className={cn("mt-4", className)}>
             <div className='flex gap-2 divide-x-2'>
                 <Input
                     value={input}
